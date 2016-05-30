3278bdcb20f203cd5830813c900c2a76e77bcb90
1 <1@DNS>
2016-05-30 20:41:44 +0300

    Copy 2 and 3 lab

---
+++ Laba1/src/class1.java
@@ -0,0 +1,24 @@
+
+public class class1 {
+	public static void main(String[] args) {
+		
+
+	int i = 5, j = 10;
+	String str = "My first string";
+	System.out.println(str);
+	System.out.println("Some text:"+str);
+	int k = i+j;
+	System.out.println("Sum:"+k);
+	System.out.println("Sum:"+(i+j));
+	while(i<j){ System.out.println(str);             
+		i++;             
+			}
+	if(i==j)
+		{ System.out.println("i="+i+", j="+j+" i oni ravny"); } 
+			else {             
+				System.out.println("i="+i+", j="+j+" i oni ne ravny"); }
+	
+	System.out.println(args[0]);
+	
+	}
+}
